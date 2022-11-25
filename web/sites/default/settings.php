283a03ad354cd8d24c3282900b30563b164dabfb
Kevin Porras <kevin.porras@pantheon.io>
2022-11-25 14:07:07 -0600

    Try using class.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -1,5 +1,7 @@
 <?php
 
+use PantheonSystems\CustomerSecrets\CustomerSecrets;
+
 /**
  * Load services definition file.
  */
@@ -32,3 +34,7 @@ $local_settings = __DIR__ . "/settings.local.php";
 if (file_exists($local_settings)) {
   include $local_settings;
 }
+
+$secrets_client = CustomerSecrets::create();
+var_dump($secrets_client)->getSecrets();
+exit();
