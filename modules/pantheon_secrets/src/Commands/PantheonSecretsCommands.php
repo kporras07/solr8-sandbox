9b7baf2e0f5b332b6b7c5452cbeafaf45a0f9b5a
Kevin Porras <kevin.porras@pantheon.io>
2023-04-26 07:24:33 -0600

    Change drush command messaging.

--- modules/pantheon_secrets/src/Commands/PantheonSecretsCommands.php
+++ modules/pantheon_secrets/src/Commands/PantheonSecretsCommands.php
@@ -19,23 +19,27 @@ class PantheonSecretsCommands extends DrushCommands {
 
   /**
    * PantheonSecretsCommands constructor.
-   */  
+   */
   public function __construct(SecretsSyncerInterface $secrets_syncer) {
     $this->secretsSyncer = $secrets_syncer;
   }
 
   /**
    * Sync Pantheon secrets with key entities.
-   * 
+   *
    * @command pantheon-secrets:sync
    */
   public function sync() {
-    $success = $this->secretsSyncer->sync();
-    if ($success) {
-      $this->logger()->success(dt('Pantheon secrets synced successfully.'));
+    try {
+      $added = $this->secretsSyncer->sync();
+      if (empty($added)) {
+        $this->logger()->notice(dt('No new secrets to sync.'));
+        return;
+      }
+      $this->logger()->success(dt('Synced secrets: @secrets', ['@secrets' => implode(', ', $added)]));
     }
-    else {
-      $this->logger()->error(dt('Pantheon secrets sync failed.'));
-    }
+    catch (\Exception $e) {
+      $this->logger()->error(dt('An error ocurred adding secrets: @error', ['@error' => $e->getMessage()]));
   }
+
 }
--- modules/pantheon_secrets/src/SecretsSyncer/SecretsSyncerInterface.php
+++ modules/pantheon_secrets/src/SecretsSyncer/SecretsSyncerInterface.php
@@ -3,15 +3,16 @@
 namespace Drupal\pantheon_secrets\SecretsSyncer;
 
 /**
- * Interface SecretsSyncerInterface.
- *
- * @package Drupal\pantheon_secrets\SecretsSyncer
+ * Interface for the service that syncs secrets.
  */
 interface SecretsSyncerInterface {
 
-    /**
-     * Sync secrets from Pantheon to key entities.
-     */
-    public function sync(): bool;
+  /**
+   * Sync secrets from Pantheon to key entities.
+   *
+   * @return array
+   *   An array of saved key entity ids.
+   */
+  public function sync(): array;
 
 }
